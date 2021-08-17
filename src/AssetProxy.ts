538cc6cf266a4280e76bee2aa59304e2cd489455
ake_boshi <327547931@qq.com>
2021-08-17 09:25:35 +0800

    update loader code

--- src/AssetProxy.ts
+++ src/AssetProxy.ts
@@ -1,10 +1,21 @@
 namespace fgui {
+
+    export enum LoaderType {
+        IMAGE = "image",
+        ATLAS = "atlas",
+        AUDIO = "audio",
+        VIDEO = "video",
+        JSON = "json",
+        SCRIPT = "script",
+        GLSL = "glsl",
+        BITMAPFONT = "bitmapFont",
+        SPRITESHEET = "spritesheet",
+
+    }
     export class AssetProxy {
-
-        private _asset: any;
-
+        private _resMap: Map<string, string>;
         constructor() {
-            this._asset = Laya.loader;
+            this._resMap = new Map();
         }
 
         private static _inst: AssetProxy;
@@ -15,16 +26,44 @@ namespace fgui {
             return AssetProxy._inst;
         }
 
-        public getRes(url: string): any {
-            return this._asset.getRes(url);
+        public getRes(key: string): string {
+            return this._resMap.get(key);
         }
 
-        public load(url: any, complete?: Laya.Handler, progress?: Laya.Handler, type?: string, priority?: number, cache?: boolean): void {
-            this._asset.load(url, complete, progress, type, priority, cache);
-        }
-
-        public setAsset(asset: any): void {
-            this._asset = asset;
+        public load(key: string, url: any, type: string, completeCallBack: Function): void {
+            this._resMap.set(key, url);
+            switch (type) {
+                case LoaderType.IMAGE:
+                    GRoot.inst.scene.load.image(key, url);
+                    break;
+                case LoaderType.ATLAS:
+                    GRoot.inst.scene.load.atlas(key, url);
+                    break;
+                case LoaderType.AUDIO:
+                    GRoot.inst.scene.load.audio(key, url);
+                    break;
+                case LoaderType.VIDEO:
+                    GRoot.inst.scene.load.video(key, url);
+                    break;
+                case LoaderType.JSON:
+                    GRoot.inst.scene.load.json(key, url);
+                    break;
+                case LoaderType.SCRIPT:
+                    GRoot.inst.scene.load.script(key, url);
+                    break;
+                case LoaderType.GLSL:
+                    GRoot.inst.scene.load.glsl(key, url);
+                    break;
+                case LoaderType.BITMAPFONT:
+                    GRoot.inst.scene.load.bitmapFont(key, url);
+                    break;
+                case LoaderType.SPRITESHEET:
+                    GRoot.inst.scene.load.spritesheet(key, url);
+                    break;
+                default:
+                    GRoot.inst.scene.load.image(key, url);
+                    break;
+            }
         }
     }
 }
